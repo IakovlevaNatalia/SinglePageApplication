c5d322bc18feb54a5f12b103d4ba723287038ca0
Natalia Balakov <69373349+IakovlevaNatalia@users.noreply.github.com>
2024-03-27 22:11:58 -0400

    work in progress

---
+++ showcase-solution/js/script.js
@@ -0,0 +1,11 @@
+$(function () { // Same as document.addEventListener("DOMContentLoaded"...
+console.log("хуй");
+    // Same as document.querySelector("#navbarToggle").addEventListener("blur",...
+    $("#navbarToggle").blur(function (event) {
+      var screenWidth = window.innerWidth;
+      if (screenWidth < 768) {
+        $("#collapsable-nav").collapse('hide');
+      }
+    });
+});
+  
